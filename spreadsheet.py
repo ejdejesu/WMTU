7cb9761694d5a7d2b1a62212fb1931604c7a30d4
Evan <evanjdejesus@gmail.com>
2018-03-02 00:35:27 -0500

    improve speed, add genre column

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,4 +1,5 @@
 import time
+import pygn
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from profanity import profanity
@@ -10,62 +11,79 @@ creds = ServiceAccountCredentials.from_json_keyfile_name(
     '/home/evanj/python/client_secret.json', scope)
 client = gspread.authorize(creds)
 
+# pygn info
+clientID = '29007503-98DC3B84B17D61D50C67122A6F46BD1E'
+userID = '51046582983290877-17F88B114B7ED6E2C1A777FC3777E703'
+
 # Open Setlist spreadsheet
 setlist = client.open("Setlist")
 
 # begin timing
 start = time.time()
 
+count = 0
+
 # Check all worksheets
 for s in range(len(setlist.worksheets())):
 
     # Get current worksheet
     sheet = setlist.get_worksheet(s)
-
-		# print str(sheet.cell(1, 5).value + " - " + sheet.title
-
     if str(sheet.cell(4, 7).value) == 'FALSE':
         continue
 
     # Get all tuples from sheet
-    songs=sheet.get_all_values()
-
+    songs = sheet.get_all_values()
+    
     # Number of checked songs
-    count=0
 
     print 'Checking setlist ' + sheet.title
 
     # Check all songs in worksheet
+    i = 0
     for i in range(len(songs)-1, 0, -1):
+        updated = False
 
+         # Update location
+        if not songs[i][5]:
+            sheet.update_cell(i+1, 6, "not yet added")
+            updated = True
 
-         # Update column 5
-        if not str(sheet.cell(i+1, 5).value):
-            sheet.update_cell(i+1, 5, "not yet added")
+        artist = songs[i][0]
+        track = songs[i][1]
+        contains = ""
+
+        # Update genre
+        if not songs[i][3]:
+            updated = True
+            metadata = pygn.search(
+                clientID=clientID, userID=userID, artist=str(artist), track=str(track))
+            genres = metadata['genre']
+            output = ""
+            for j in range(len(genres)):
+                value = str(genres[str(j+1)]['TEXT'])
+                output += (value if value != 'Alternative & Punk' else 'Alternative') + (', ' if j<len(genres)-1 else "")
+            sheet.update_cell(i+1, 4, output)
 
         # Only check if status is not listed
-        if str(sheet.cell(i+1, 4).value):
-            continue
+        if not songs[i][4]:
+            updated = True
 
-        artist=songs[i][0]
-        track=songs[i][1]
-        contains=""
+            # Try to find lyrics and profanity level
+            try:
+                lyrics = PyLyrics.getLyrics(artist, track)
+                contains = 'profane' if profanity.contains_profanity(
+                    lyrics) else 'clean'
+                print 'Song ' + str(i) + ' updated'
+            except ValueError:
+                contains = "unknown"
 
-        # Try to find lyrics and profanity level
-        try:
-            lyrics=PyLyrics.getLyrics(artist, track)
-            contains='profane' if profanity.contains_profanity(
-                lyrics) else 'clean'
-            print 'Song ' + str(i) + ' checked'
-        except ValueError:
-            contains="unknown"
+            # Update column 5 of each song with profanity value
+            sheet.update_cell(i+1, 5, contains)
 
-        # Update column 4 of each song with profanity value
-        sheet.update_cell(i+1, 4, contains)
-        count=count + 1
-
-    print 'Checked ' + str(count) + ' songs in setlist ' + sheet.title
+        if updated:
+            count = count + 1
 
 # end timing
-end=time.time()
-print 'elapsed time: ' + str(end - start) + ' s'
+end = time.time()
+print 'Updated ' + str(count) + ' songs'
+print 'elapsed time: %.2f s' % (end-start)
